d2a2487743082fe48ebbbff15c01e9574f84a070
Stetson Done <scdone@gmail.com>
2021-12-03 07:38:36 -0700

    SLDC exercise done.

--- sldc.js
+++ sldc.js
@@ -13,7 +13,36 @@
 // Testing & Integration
 
 
+// REORGANIZED IN ORDER:
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
 */
+
+let planning = ("PLANNING is taking time to decide what problem needs to be solved \nand how to solve it from a high-level perspective.")
+let analysisOfRequirements = ("ANALYSIS OF REQUIREMENTS is finding and defining what parameters \nwe need to work within and what expectations need to be met \nwhile solving the problem.")
+let design = ("DESIGN is making a layout and more specific plan for what needs to be done. \nPlanning the actual code with the expected appearance and function of final product.")
+let implementation = ("IMPLEMENTATION is writing the code according to the design we have planned \nand putting it into our system or project.")
+let testingAndIntegration = ("TESTING AND INTEGRATION is putting the code into the framework \nto see if it meets expectations. Bugs may be found here and will need to be reworked.")
+let maintain = ("MAINTAINING is the continual assessment of the code. \nAs the project grows and is added to over time, or as it is put through more use, \nbugs may become apparent which we did not see at first. \nContinual maintenance is necessary to ensure our solution is always working properly. ")
+
+console.log(planning)
+
+console.log(analysisOfRequirements)
+
+console.log(design)
+
+console.log(implementation)
+
+console.log(testingAndIntegration)
+
+console.log(maintain)
+
