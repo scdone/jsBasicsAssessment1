fee01b2d3ec1b8bcc1830003c19ad854128887b6
Stetson Done <scdone@gmail.com>
2021-12-03 08:46:59 -0700

    arrays prblems 1-3 done, working on problem 4.

--- arrays.js
+++ arrays.js
@@ -5,10 +5,15 @@ const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
 
+let colorCopy = faveColors.slice(0,3)
+console.log(colorCopy)
+
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+colorCopy.push("yellow")
+console.log(colorCopy)
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -16,9 +21,12 @@ const faveColors = ['red', 'green', 'black']
   numbers array that will capture only the middle numbers (2, 3, 4).
 */
 
-const numbers = [1, 2, 3, 4, 5]
-
 //CODE HERE
+
+const numbers = [1, 2, 3, 4, 5]
+let middleNums = numbers.slice(1,4)
+
+console.log(middleNums)
 
 
 //////////////////PROBLEM 4////////////////////
