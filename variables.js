5b7709991bf351a06c9a066cd836f25a6b263ad3
Stetson Done <scdone@gmail.com>
2021-12-03 07:55:02 -0700

    variables final commit

--- variables.js
+++ variables.js
@@ -3,15 +3,24 @@
 
 //CODE HERE 
 
+let myName = "Stetson"
+console.log(myName)
+
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
 
+let faveNum = 7
+console.log(faveNum)
+
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
+
+let lovesCode = true
+console.log(lovesCode)
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -22,9 +31,19 @@
 
 //CODE HERE
 
+if(faveNum === 13) {
+  console.log(`Lucky 13!`)
+} else {
+  console.log(`Not lucky 13.`)
+}
+
 //////////////////PROBLEM 5////////////////////
 /*
   Write a for loop that prints out your faveNum 5 times.
 */
 
 //CODE HERE
+
+for (i = 0; i < 5; i++){
+  console.log(faveNum)
+}
